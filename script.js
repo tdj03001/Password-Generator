8e90b20ffcd89fa15b87aa61c1a4fddc2d49c06c
Tyler Jenkins <tyjenki@gmail.com>
2019-12-04 15:42:09 -0500

    got 'undefined' to show up in textarea. Getting closer!

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function createPassword() {
-  var pword = prompt("How many characters would you like your password to have (must be between 8 and 128)?");
+function promptUser() {
+  var charCountChoice = prompt("How many characters would you like your password to have (must be between 8 and 128)?");
+  var includeSpecial = confirm("Click OK to confirm including special characters in your password.");
+  var includeNumeric = confirm("Click OK to confirm including numeric characters in your password.");
+  var includeLowerCase = confirm("Click OK to confirm including lower case characters in your password.");
+  var includeUpperCase = confirm("Click OK to confirm including upper case characters in your password.");
+  
+  console.log(charCountChoice);
+  console.log(includeSpecial);
+  console.log(includeNumeric);
+  console.log(includeLowerCase);
+  console.log(includeUpperCase);  
 }
+  
+var specialChar= ["!", "#", "$", "%", "^", "&", "*", "-", "+"];
+var numericChar= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+
+
+
+
+var password= promptUser();
+
+function showPassword() {
+  return password;
+}
+document.getElementById("textarea").innerHTML = showPassword();
