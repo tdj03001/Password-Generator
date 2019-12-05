662bb12a21a6781571b42a287de9cf65c3ad3d5c
Tyler Jenkins <tyjenki@gmail.com>
2019-12-04 20:18:34 -0500

    further testing revealed any response to initial prompt caused the error message and the function to rerun. Resolved this.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 function promptUser() {
   var charCountChoice = prompt("How many characters would you like your password to have (must be between 8 and 128)?");
-    if (charCountChoice < 8 || charCountChoice > 128 || charCountChoice.typeOf!==Number) {
+    if (charCountChoice < 8 || charCountChoice > 128 || isNaN(charCountChoice)) {
       alert("Entry must be an integer between 8 and 128.");
       promptUser();
+    } else {
+      var includeSpecial = confirm("Click OK to confirm including special characters in your password.");
+      var includeNumeric = confirm("Click OK to confirm including numeric characters in your password.");
+      var includeLowerCase = confirm("Click OK to confirm including lower case characters in your password.");
+      var includeUpperCase = confirm("Click OK to confirm including upper case characters in your password.");
     }
-  var includeSpecial = confirm("Click OK to confirm including special characters in your password.");
-  var includeNumeric = confirm("Click OK to confirm including numeric characters in your password.");
-  var includeLowerCase = confirm("Click OK to confirm including lower case characters in your password.");
-  var includeUpperCase = confirm("Click OK to confirm including upper case characters in your password.");
-  
+    
   console.log(charCountChoice);
   console.log(includeSpecial);
   console.log(includeNumeric);
@@ -20,13 +21,13 @@ function promptUser() {
   document.getElementById("textarea").innerHTML = showPassword();
 }
   
-var specialChar= ["!", "#", "$", "%", "^", "&", "*", "-", "+"];
-var numericChar= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperChar= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "#", "$", "%", "^", "&", "*", "-", "+"];
+var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
 
-var password= undefined;
+var password = undefined;
 
 function showPassword() {
   return password;
